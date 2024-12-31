f4ea7d898b7738c7e894261e7e8762acc6a4406e
Tomer Norman <spidernor38@gmail.com>
2024-12-31 13:38:53 +0200

    Added middleware redirect

--- webapp/src/middleware.ts
+++ webapp/src/middleware.ts
@@ -1,6 +1,15 @@
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-    // app should redirect to /dashboard by default
-    return
+  const isPageRequest = !request.nextUrl.pathname.match(/\.(js|css|png|jpg|svg)$/);
+
+  if (request.nextUrl.pathname === "/dashboard") {
+    return NextResponse.next();
+  }
+
+  if (isPageRequest) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
 }
